505daacd5c65298090506a46b8cbc766a4b5b919
David Gilkeson <davidgilkeson8@gmail.com>
2020-05-17 23:55:20 +0100

    Initial commit Keeper app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const name = "David Gilkeson";
+const date = new Date().getFullYear();
+
+return (
+  <div>
+    <p>Created by {name};</p>
+    <p>Copyright {year}</p>
+  </div>
+);
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+return (
+  <h1 className="heading" style={customStyle}>
+    {" "}
+    {greeting}{" "}
+  </h1>
+);
+
+export default Heading;
